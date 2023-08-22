59750ccd6b177d5cf0a52ea551977002cc92dc1a
Nutty-Nuts <vegagerard1130@gmail.com>
2023-08-22 13:48:30 +0800

    added font awesome

--- media-setup.sh
+++ media-setup.sh
@@ -2,4 +2,4 @@
 
 sudo pacman -S pipewire pipewire-pulse wireplumber
 sudo pacman -S firefox chromium spotify-launcher
-sudo pacman -S noto-fonts noto-fonts noto-fonts-emoji noto-fonts-extra noto-fonts-cjk
+sudo pacman -S noto-fonts noto-fonts noto-fonts-emoji noto-fonts-extra noto-fonts-cjk ttf-font-awesome
